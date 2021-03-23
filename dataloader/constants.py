6dd1b469b67e85a5035337c5f58e91e843498513
Vishnu Dutt Sharma <vishnusharma619@gmail.com>
2021-03-22 22:59:55 -0400

    Updated parameters

--- dataloader/constants.py
+++ dataloader/constants.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 NUM_ROBOT = 10 #number of robots
-REWARD_THRESH = 0.70 # mimimum value of a reward
+REWARD_THRESH = 0.97 # mimimum value of a reward
 PATH_LEN = 2 # Path length/Time horizon
 HEIGHT = 100 # Height of the grid
 WIDTH = 100 # Width of the grid
 GRID_SIZE = HEIGHT
-STEP = 6
-FOV = 3
+STEP = 12# 6
+FOV = 6#3
 NUM_TGT_FEAT = 20
 NUM_ROB_FEAT = 10
 COMM_RANGE = 20.
