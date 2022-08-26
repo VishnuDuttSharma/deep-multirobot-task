6caff7a5b6dae95bd7c555f6b4bf581d41e02bac
Vishnu Sharma <vishnuds@umiacs.umd.edu>
2022-08-26 15:50:54 -0400

    Removed unwanted code. Added options to allow integration with existing code

--- get_results.py
+++ get_results.py
@@ -33,24 +33,25 @@ arg_parser.add_argument(
     default='None',
     help='The Configuration file in json format')
 
-arg_parser.add_argument('--mode', type=str, default='train')
+arg_parser.add_argument('--mode', type=str, default='test', help="Mode: train/test. 'test' is default")
 arg_parser.add_argument('--log_time_trained', type=str, default='0')
-arg_parser.add_argument('--timeid', type=str, default=None)
+arg_parser.add_argument('--timeid', type=str, default=None, help='Time identifier to load the model')
+arg_parser.add_argument('--load_epoch', type=int, default=-1, help="Epoch number (multiple of 500) to be used for testing. If not given, latest model will be used") 
 
-arg_parser.add_argument('--tgt_feat', type=int, default=20)
-arg_parser.add_argument('--rbt_feat', type=int, default=10)
+arg_parser.add_argument('--tgt_feat', type=int, default=20, help="Number of targets in feature vector. Default: 20")
+arg_parser.add_argument('--rbt_feat', type=int, default=10, help="Number of neighboring robots in feature vector. Default: 20")
 
-arg_parser.add_argument('--num_agents', type=int, default=10)
+arg_parser.add_argument('--num_agents', type=int, default=20, help="Number of robots. Default 20")
 arg_parser.add_argument('--map_w', type=int, default=20)
 arg_parser.add_argument('--map_density', type=int, default=1)
 arg_parser.add_argument('--map_type', type=str, default='map')
 
-arg_parser.add_argument('--trained_num_agents', type=int, default=10)
+arg_parser.add_argument('--trained_num_agents', type=int, default=20)
 arg_parser.add_argument('--trained_map_w', type=int, default=20)
 arg_parser.add_argument('--trained_map_density', type=int, default=1)
 arg_parser.add_argument('--trained_map_type', type=str, default='map')
 
-arg_parser.add_argument('--nGraphFilterTaps', type=int, default=0)
+arg_parser.add_argument('--nGraphFilterTaps', type=int, default=2, help="Number of communication filtertaps i.e. number of hops+1")
 arg_parser.add_argument('--hiddenFeatures', type=int, default=0)
 
 arg_parser.add_argument('--num_testset', type=int, default=4500)
@@ -74,64 +75,18 @@ config = process_config(args)
 # print(config)
 
 config['device'] = torch.device('cuda:0')
-config.tgt_feat = 20 #40
-config.rbt_feat = 10
-config.max_epoch = 1500 #3000
-config.learning_rate = 0.005
-config.nGraphFilterTaps = 2
-
 
 timeid = args.timeid
 
-# change line 22 and line 24 in graphs/models/coverageplanner.py
 
 # Create the Agent and pass all the configuration to it then run it..
 agent_class = globals()[config.agent]
 agent = agent_class(config)
 
-'''
-print('Plotting graphs')
-
-event_acc = EventAccumulator(f'/home/vishnuds/baxterB/multi_robot/gnn_tb_data/dcpOE_map20x20_rho1_{config.num_agents}Agent/K{config.nGraphFilterTaps}_HS0/{timeid}/')
-event_acc.Reload()
-
-
-train_batch_size = config.batch_size
-if config.num_agents == 50:
-    train_data_size = 29500 #60000
-elif config.num_agents == 20:
-    train_data_size = 300000# 120000 
-else:
-    train_data_size = 240000 #59000
-    
-steps = np.array(event_acc.Scalars('iteration/loss'))[:,1].astype(int)*train_batch_size // train_data_size
-train_loss = np.array(event_acc.Scalars('iteration/loss'))[:,2]
-
-train_steps = []
-train_loss_4_plot = []
-for idx in np.unique(steps):
-    train_steps.append(idx)
-    train_loss_4_plot.append( np.mean(train_loss[steps == idx]) )  
-
-
-
-valid_step = np.array(event_acc.Scalars('epoch/loss_validStep'))[:,1].astype(int)
-valid_loss = np.array(event_acc.Scalars('epoch/loss_validStep'))[:,2]
-
-
-# fig, ax = plt.subplots( nrows=1, ncols=1 ) 
-plt.plot(train_steps, train_loss_4_plot, 'r', label='Training loss')
-plt.plot(valid_step, valid_loss, 'b', label='Validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Cross-Ent Loss')
-plt.legend()
-#plt.show()
-plt.savefig('results.png', bbox_inches='tight')
-print('Done plotting')
-'''
-### /home/vishnuds/baxterB/multi_robot/gnn_log_data/experiments/dcpOE_map20x20_rho1_10Agent/K4_HS0/1617748311/
-
-filename = f'{config.save_data}/experiments/dcpOE_map20x20_rho1_{config.num_agents}Agent/K{config.nGraphFilterTaps}_HS0/{timeid}/checkpoints/checkpoint_{config.max_epoch}.pth.tar'
+if args.load_epoch >= 0:
+    filename = f'{config.save_data}/experiments/dcpOE_map20x20_rho1_{config.num_agents}Agent/K{config.nGraphFilterTaps}_HS0/{timeid}/checkpoints/checkpoint_{args.load_epoch}.pth.tar'
+else:    
+    filename = f'{config.save_data}/experiments/dcpOE_map20x20_rho1_{config.num_agents}Agent/K{config.nGraphFilterTaps}_HS0/{timeid}/checkpoints/checkpoint_{config.max_epoch}.pth.tar'
 # filename = '/home/vishnuds/baxterB/multi_robot/gnn_log_data/dummy_model/checkpoint_500.pth.tar'
 print(f'loading model from: {filename}')
 checkpoint = torch.load(filename, map_location='cuda:{}'.format(agent.config.gpu_device))
@@ -152,26 +107,3 @@ print(f'Test Accuracy: {100*acc}%')
 print(f'Test Loss: {lss}')
 #######################################################
 
-'''
-#######################################################
-acc = get_stoc_acc(config, agent, agent.data_loader.train_loader)
-print(f'Train Accuracy(stoc): {100*acc}%')
-
-acc = get_stoc_acc(config, agent, agent.data_loader.validStep_loader)
-print(f'Valid Accuracy(stoc): {100*acc}%')
-
-acc = get_stoc_acc(config, agent, agent.data_loader.test_loader)
-print(f'Test Accuracy(stoc): {100*acc}%')
-#######################################################
-
-#######################################################
-acc = get_stoc_acc2(config, agent, agent.data_loader.train_loader)
-print(f'Train Accuracy(stoc): {100*acc}%')
-
-acc = get_stoc_acc2(config, agent, agent.data_loader.validStep_loader)
-print(f'Valid Accuracy(stoc): {100*acc}%')
-
-acc = get_stoc_acc2(config, agent, agent.data_loader.test_loader)
-print(f'Test Accuracy(stoc): {100*acc}%')
-########################################################
-'''
